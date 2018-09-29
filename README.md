86dd4e3eb1b1d65d9b91a58056bf17e21373b6c9
Dean <deanpatrickgaffney@gmail.com>
2018-09-29 16:57:44 +0100

    added conf file to code, added readme

---
+++ README.md
@@ -0,0 +1,36 @@
+<div style="background-image:url(./images/iris_jumbo_bg.png); background-color:black;">
+    <div style="position: relative; left: 0; top: 0;">
+        <img src="./images/iris_logo_colour.png" style="position: relative; top: 0; left: 0;"/>
+    </div>
+</div>
+
+# Iris MySQL Agent
+
+This repository contains the MySQL agent for my final year project [Iris](https://github.com/DeanGaffney/iris). This agent uses a python script to get the memory usage of a specific database. The memory usage and database name is then sent to Iris where it is displayed on a chart in real time. This agent was deployed on the same EC2 instance as the Iris web application and was run every day using cron to keep track of Iris' database memory.
+
+## Agent Schema
+The following is the schema that was created in Iris for the agent.
+![Iris UI Schema](./images/iris-mysql-schema.PNG)
+
+## Agent Dashboard
+The following image shows the dashboard for the agent inside Iris where it is keeping track of the Iris database memory.
+![Iris Dashboard](./images/iris-mysql-dashboard.jpeg)
+
+## Agent Transformation Rule
+This agent has no transformation rule, please see the [Node.js Agent](https://github.com/DeanGaffney/iris-node) for an example of a transformation rule.
+
+## Other Iris agents
+* [Selenium](https://github.com/DeanGaffney/iris-selenium)
+* [Node.js](https://github.com/DeanGaffney/iris-node)
+* [Android](https://github.com/DeanGaffney/iris-android)
+* [Crypto Currency Rates](https://github.com/DeanGaffney/iris-crypto-rates)
+
+## Built With
+
+- Python
+
+## Authors
+
+* **Dean Gaffney**
+
+See also the list of [contributors](https://github.com/DeanGaffney/iris-selenium/graphs/contributors) who participated in this project.
--- iris-mysql.py
+++ iris-mysql.py
@@ -15,33 +15,29 @@ urlResp = requests.post('http://ec2-52-16-53-220.eu-west-1.compute.amazonaws.com
 
 endpoint = urlResp.json()['url']
 
-config = {
-    'user': 'root',
-    'password': 'iris',
-    'host': 'ec2-52-16-53-220.eu-west-1.compute.amazonaws.com',
-    'database': 'iris'
-}
+with open('./conf.json') as conf_file:
+    config = json.load(conf_file)["database"]
 
-cnx = cur = None
-try:
-    cnx = mysql.connector.connect(**config)
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print('Something is wrong with your user name or password')
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+    cnx = cur = None
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Something is wrong with your user name or password')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
     else:
-        print(err)
-else:
-    cur = cnx.cursor()
-    cur.execute('SELECT table_schema AS "databaseName", ROUND(SUM(data_length + index_length) / 1024 /  1024, 2) as "memoryMB" FROM information_schema.TABLES GROUP BY table_schema;')
-    row_headers=[x[0] for x in cur.description] #this will extract row headers
-    rv = cur.fetchall()
-    for result in rv:
-        resp = requests.post(endpoint, data=json.dumps(dict(zip(row_headers,result))), headers=headers)
-        print(resp.json())
-finally:
-    if cur:
-        cur.close()
-    if cnx:
-        cnx.close()
+        cur = cnx.cursor()
+        cur.execute('SELECT table_schema AS "databaseName", ROUND(SUM(data_length + index_length) / 1024 /  1024, 2) as "memoryMB" FROM information_schema.TABLES GROUP BY table_schema;')
+        row_headers=[x[0] for x in cur.description] #this will extract row headers
+        rv = cur.fetchall()
+        for result in rv:
+            resp = requests.post(endpoint, data=json.dumps(dict(zip(row_headers,result))), headers=headers)
+            print(resp.json())
+    finally:
+        if cur:
+            cur.close()
+        if cnx:
+            cnx.close()
